575635f13a703c140656a37248fa1701a66108b7
kartik <vermakartik222@gmail.com>
2020-04-01 10:58:29 +0530

    adding slider style

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ColorBox from "./ColorBox";
-import "./Palette.css";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
+import "./Palette.css";
 
 class Palette extends React.Component {
     constructor(props) {
@@ -20,15 +20,17 @@ class Palette extends React.Component {
     }
 
     render() {
-        const colors = this.props.palette.colors[this.state.level].map(color => (
-            <ColorBox key={color.name} name={color.name} background={color.hex} />
-        ));
+        let { colors } = this.props.palette;
+        let { level } = this.state;
+        const color = colors[level].map(color => <ColorBox key={color.name} name={color.name} background={color.hex} />);
 
         return (
             <div className="Palette">
                 {/* Navbar goes here */}
-                <Slider min={100} max={900} step={100} defaultValue={this.state.level} onAfterChange={this.sliderChange} />
-                <div className="Palette-colors">{colors}</div>
+                <div className="slider">
+                    <Slider min={100} max={900} step={100} defaultValue={level} onAfterChange={this.sliderChange} />
+                </div>
+                <div className="Palette-colors">{color}</div>
                 {/* footer goes here */}
             </div>
         );
